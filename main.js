45bb71f944066173f12896359ce8532cab3129e8
Simona Cartolina <simonacartolina@hotmail.com>
2023-10-10 18:23:19 +0200

    modified like-button events

--- main.js
+++ main.js
@@ -74,46 +74,153 @@ al clicco del bottone, il bottone diventa rosso
 
 */
 
-let likeButton=document.querySelector('a.like-button');
-let likeCounter=parseInt(document.getElementById('like-counter-1').textContent);
+// Seleziona tutti i pulsanti like
 
-/*2)  Prendo il bottone like
-prendo il bottone counter like
-faccio un add event listener sul bottone like
-quando lo clicco, il counter like diventa quello che è adesso + 1*/
 
-likeButton.addEventListener('click', function(){
-    likeButton.style.color='red';
 
-    likeCounter++;
-    document.getElementById('like-counter-1').textContent = likeCounter;
-});
+function renderPosts() {
+    // Get the container element
+    const container = document.getElementById("container");
 
-let container=document.getElementById('container');
+    // Iterate through the posts array
+    for (const post of posts) {
+        // Create a post div element
+        const postDiv = document.createElement("div");
+        postDiv.className = "post";
 
-posts.forEach(function(card) {
-    // Crea un elemento HTML per la card
-    let cardElement = document.createElement("div");
-    cardElement.classList.add("post");
+        // Create the post header div
+        const headerDiv = document.createElement("div");
+        headerDiv.className = "post__header";
 
-    // Crea gli elementi interni per il contenuto della card
-    let titleElement = document.createElement("h1");
-    titleElement.textContent = card.author;
-    titleElement.classList.add('post-meta__author')
-    titleElement.classList.add('post-meta__author')
-    let descriptionElement = document.createElement("p");
-    descriptionElement.classList.add('post__text');
+        // Create the post meta div
+        const metaDiv = document.createElement("div");
+        metaDiv.className = "post-meta";
 
-    descriptionElement.textContent = card.content;
-    let imageElement = document.createElement("img");
-    imageElement.src = card.media;
-    imageElement.classList.add('scoreimg');
+        // Create the post meta icon div
+        const iconDiv = document.createElement("div");
+        iconDiv.className = "post-meta__icon";
 
-    // Aggiungi gli elementi interni alla card
-    cardElement.appendChild(titleElement);
-    cardElement.appendChild(descriptionElement);
-    cardElement.appendChild(imageElement);
+        // Create the author's profile pic
+        const profilePic = document.createElement("img");
+        profilePic.className = "profile-pic";
+        profilePic.src = post.author.image || "https://unsplash.it/300/300?image=15";
+        profilePic.alt = post.author.name;
 
-    // Aggiungi la card al contenitore
-    container.appendChild(cardElement);
-});
+        // Create the post meta data div
+        const dataDiv = document.createElement("div");
+        dataDiv.className = "post-meta__data";
+
+        // Create the author's name
+        const authorDiv = document.createElement("div");
+        authorDiv.className = "post-meta__author";
+        authorDiv.textContent = post.author.name;
+
+        // Create the post creation time
+        const timeDiv = document.createElement("div");
+        timeDiv.className = "post-meta__time";
+        timeDiv.textContent = "4 mesi fa"; // You can format the date as needed
+
+        // Append elements to their respective parents
+        iconDiv.appendChild(profilePic);
+        dataDiv.appendChild(authorDiv);
+        dataDiv.appendChild(timeDiv);
+        metaDiv.appendChild(iconDiv);
+        metaDiv.appendChild(dataDiv);
+        headerDiv.appendChild(metaDiv);
+
+        // Create the post text div
+        const textDiv = document.createElement("div");
+        textDiv.className = "post__text";
+        textDiv.textContent = post.content;
+
+        // Create the post image div
+        const imageDiv = document.createElement("div");
+        imageDiv.className = "post__image";
+
+        // Create the post image
+        const postImage = document.createElement("img");
+        postImage.src = post.media;
+        postImage.alt = "";
+
+        // Append the post image to its parent div
+        imageDiv.appendChild(postImage);
+
+        // Create the post footer div
+        const footerDiv = document.createElement("div");
+        footerDiv.className = "post__footer";
+
+
+
+        // Create the likes CTA div
+        const likesCtaDiv = document.createElement("div");
+        likesCtaDiv.className = "likes__cta";
+
+        // Create the like button
+        const likeButton = document.createElement("a");
+        likeButton.className = "like-button js-like-button";
+        likeButton.href = "#";
+        likeButton.setAttribute("data-postid", post.id);
+
+        // Create the like button icon
+        const likeButtonIcon = document.createElement("i");
+        likeButtonIcon.className = "like-button__icon fas fa-thumbs-up";
+        likeButtonIcon.setAttribute("aria-hidden", "true");
+
+        // Create the like button label
+        const likeButtonLabel = document.createElement("span");
+        likeButtonLabel.className = "like-button__label";
+        likeButtonLabel.textContent = "Mi Piace";
+
+        // Append elements to their respective parents
+        likeButton.appendChild(likeButtonIcon);
+        likeButton.appendChild(likeButtonLabel);
+        likesCtaDiv.appendChild(likeButton);
+
+        // Create the likes counter div
+        const likesCounterDiv = document.createElement("div");
+        likesCounterDiv.className = "likes__counter";
+        likesCounterDiv.innerHTML = `Piace a <b id="like-counter-${post.id}" class="js-likes-counter">${post.likes}</b> persone`;
+
+        // Append elements to their respective parents
+        likesCtaDiv.appendChild(likesCounterDiv);
+        footerDiv.appendChild(likesCtaDiv);
+
+        // ... (rest of the code)
+
+        // Append all elements to the post div
+        postDiv.appendChild(headerDiv);
+        postDiv.appendChild(textDiv);
+        postDiv.appendChild(imageDiv);
+        postDiv.appendChild(footerDiv);
+        postDiv.appendChild(likesCtaDiv);
+
+        // Append the post div to the container
+        container.appendChild(postDiv);
+    }
+
+    // Add event listeners to all like buttons after they are created
+    const likeButtons = document.querySelectorAll('a.like-button');
+    likeButtons.forEach(function (likeButton) {
+        likeButton.addEventListener('click', function (event) {
+            event.preventDefault();
+            const postId = likeButton.getAttribute('data-postid');
+            const likeCounterElement = document.getElementById('like-counter-' + postId);
+            let likeCounter = parseInt(likeCounterElement.textContent);
+
+            // Simulate liking by toggling the color
+            if (likeButton.style.color === 'red') {
+                likeButton.style.color = '';
+                likeCounter--;
+            } else {
+                likeButton.style.color = 'red';
+                likeCounter++;
+            }
+
+            // Update the like counter
+            likeCounterElement.textContent = likeCounter;
+        })
+    })
+}
+
+// Call the renderPosts function to display the posts
+renderPosts();
